73517937881f21e62af8b4ec806946821bf0d36e
Sangamesh Biradar <sangameshbiradar586141@gmail.com>
2023-01-25 18:04:31 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const nedb = require('nedb');
-const users = new nedb({ filename: 'db/users.db', autoload: true })
+const users = new nedb({ filename: 'db/users.db', autoload: true, corruptAlertThreshold: 1 })
 const products = new nedb({ filename: 'db/products.db', autoload: true });
 
 module.exports.users = users
